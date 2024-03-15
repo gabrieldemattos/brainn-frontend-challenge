1e97012917c256f49c1bbfe371a244d0ede0c791
Gabriel de Mattos <mattos_95@hotmail.com>
2024-03-15 17:26:44 -0300

    feat: Create raffle ball  component

---
+++ src/app/components/raffle-ball.tsx
@@ -0,0 +1,13 @@
+interface RaffleBallProps {
+  number: number | string;
+}
+
+const RaffleBall = ({ number }: RaffleBallProps) => {
+  return (
+    <div className="bg-white w-20 h-20 rounded-full flex items-center justify-center font-bold text-xl lg:w-24 lg:h-24 lg:text-2xl animate-from-bottom">
+      {number}
+    </div>
+  );
+};
+
+export default RaffleBall;
